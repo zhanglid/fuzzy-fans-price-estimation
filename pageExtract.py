10ec99622c5a8b2bc74589db086256575e49794d
zhangliang <zhanglid@usc.edu>
2017-04-09 23:36:17 -0700

    Update the code for extracting work info

--- pageExtract.py
+++ pageExtract.py
@@ -1,8 +1,21 @@
-import re
-
+import requests
+from bs4 import BeautifulSoup
 
 class ArtWorkPage:
     def __init__(self, url):
         self.url = url
 
-    def
+    def get_info(self):
+        try:
+            data = requests.get(self.url).content
+        except requests.exceptions, e:
+            print 'Error: ', e
+            return
+        soup = BeautifulSoup(data)
+
+        info = {
+            'title': soup.body.find('div',{'class':'titLeft'}).h1.text,
+            'authors': map(lambda t: t.text.split()[0],soup.body.find('div',{'class':'worksInfo'},id='').table.td.find_all('a'))
+            'size':
+        }
+
